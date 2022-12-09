da3719fae1adb2b263d71cf49435be9594045232
mlopj <109193876+mlopj@users.noreply.github.com>
2022-12-09 00:52:00 -0900

    Added WSL project, definitely not finished.

---
+++ projects/websocketlive/script.js
@@ -0,0 +1,29 @@
+const { time } = require("console")
+let input_element = document.getElementById('socket-name')
+function generate() {
+    // variables
+    let input = document.getElementById('socket-name').value
+    let input_element = document.getElementById('socket-name')
+    let create_btn = document.getElementById('create-btn')
+    let default_name = false
+
+    //if input is empty sets it to default
+    if (input === "") {
+        input = "Untitled Web Socket"
+        default_name = true
+    }
+
+    //makes input uneditable
+    input_element.setAttribute("readonly", "");
+
+    document.getElementById('socket-name').value = input+" The Socket ;)"
+
+    //animates button
+    create_btn.classList = "animated"
+    setTimeout(function(){
+        create_btn.innerText = "OFFLINE" 
+    }, 500);
+}
+function set() {
+    this.style.width = this.value.length + "ch";
+}
